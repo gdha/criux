5841a1754705ab834f113c96bd395fa163b69022
Gratien D'haese <gratien.dhaese@hp.com>
2015-02-26 14:10:15 +0100

    Described the cluster workflow and foreseen variables for diff cluster software; readme explains the basics

--- opt/criux/lib/cluster-workflow.sh
+++ opt/criux/lib/cluster-workflow.sh
@@ -7,7 +7,14 @@ function WORKFLOW_cluster {
     # init will initialize CURRENT_STATUS variable
     SourceStage "init"
 
+    # Stage 'cluster' (directory) is generic where we will find out the type of the cluster
     SourceStage "cluster"
+
+    # according the type we will source another stage
+    # currenly we only have serviceguard, but veritascluster, hacmp, etc.. are candidates
+    [[ "$SERVICEGUARD_CLUSTER" =~ ^[yY1] ]] && SourceStage "serviceguard"
+    [[ "$VERITAS_CLUSTER" =~ ^[yY1] ]] && SourceStage "vcs"
+    [[ "$POWERHA_CLUSTER" =~ ^[yY1] ]] && SourceStage "powerha"
 
     SourceStage "cleanup"
 }
