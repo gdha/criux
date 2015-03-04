ef574d20f8b933cd50461d5ddfe23237237068b7
Gratien D'haese <gratien.dhaese@hp.com>
2015-03-04 16:28:04 +0100

    add new script (system workflow) system/default/05_check_hostname.sh

---
+++ opt/criux/scripts/system/default/05_check_hostname.sh
@@ -0,0 +1,14 @@
+#05_check_hostname.sh
+# check if our hostname is valid and recorded in /etc/hosts file
+
+if [[ -z "$HOSTNAME"]]; then
+    PrintF 1 "==" "HOSTNAME variable has not been defined" ; NOK
+else
+    grep -q "$HOSTNAME" /etc/hosts
+    if [[ $? -eq 0 ]]; then
+        PrintF 1 "**" "HOSTNAME=$HOSTNAME defined in /etc/hosts" ; OK
+    else
+        PrintF 1 "==" "HOSTNAME=$HOSTNAME not found in /etc/hosts" ; NOK
+    fi
+fi
+
