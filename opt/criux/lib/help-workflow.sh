e5076f58427cecdf68b2d154c8fa4fa9948ab670
Gratien D'haese <gratien.dhaese@gmail.com>
2015-03-04 16:29:17 +0100

    removed some obsolete options from help workflow (were copied from upgrade-ux)

--- opt/criux/lib/help-workflow.sh
+++ opt/criux/lib/help-workflow.sh
@@ -12,10 +12,8 @@ Available options:
  -c DIR       alternative config directory; instead of /etc/opt/$PROGRAM
  -d           debug mode; log debug messages
  -D           debugscript mode; log every function call
- -F           forcing preview or upgrade to continue when entering next stage is not allowed
  -s           simulation mode; show what scripts $PROGRAM would include
  -S           step-by-step mode; acknowledge each script individually
- -p           preview mode; run all install or other commands in preview
  -v           verbose mode; show more output
  -V           version information
 
