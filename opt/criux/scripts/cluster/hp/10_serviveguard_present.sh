0be61e75616a7ae42e702a6c92f41c5e1f7a3df0
Gratien D'haese <gratien.dhaese@hp.com>
2015-03-05 16:50:30 +0100

    adding 10_serviveguard_present.sh script

---
+++ opt/criux/scripts/cluster/hp/10_serviveguard_present.sh
@@ -0,0 +1,17 @@
+# 10_sg_version_matrix.sh
+# This script verifies the version of HP ServiveGuard and defines SERVICEGUARD_CLUSTER=1 is sg is present
+# SG_VERSION= (empty as defined in default.conf)
+
+case "$OS_VERSION" in
+    11.11)         SG_VERSION=$( grep B3935DA $TMP_DIR/swlist.output| awk '{print $2}' ) ;;
+    11.23|11.31)   SG_VERSION=$( grep T1905CA $TMP_DIR/swlist.output| awk '{print $2}' ) ;;
+esac
+
+if [[ ! -z "$SG_VERSION" ]]; then
+    SERVICEGUARD_CLUSTER=1
+    LogPrint "Found Serviceguard version $SG_VERSION"
+    #PrintF " **" "HP Serviceguard version $SG_VERSION" ; OK  # OK is a function
+fi 
+
+# we will not go any deeper in this workflow; we will kick off a sub-workflow serviceguard
+# to investigate the cluster definitions and so on
