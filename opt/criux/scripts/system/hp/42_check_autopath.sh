9caee70b44ba4862b3709c959e69140f5e06a028
Gratien D'haese <gratien.dhaese@hp.com>
2015-03-17 13:26:13 +0100

    Added script for HP-UX 11.31 42_check_scsimgr_lbp.sh to checl load balancing policy; corrected some errors in 42_check_autopath.sh

--- opt/criux/scripts/system/hp/42_check_autopath.sh
+++ opt/criux/scripts/system/hp/42_check_autopath.sh
@@ -3,16 +3,34 @@
 # HP-UX 11.31 does not have autopath, it is now built-in
 
 /sbin/autopath | grep "Auto Discover" | grep -q ON
-if [[ $? -eq 0 ]]; then
+if (( $? = 0 )); then
     Ok "Secure Path Auto Discover is on."
 else
     Warn "Secure Path Auto Discover is off!"
 fi
 
-count=$(/sbin/autopath | grep "Load Balancing Policy" | uniq | grep "Shortest Service Time" | wc -l)
-if (( $count = 1 )); then
-    Ok "Autopath load balacing policy is set to SST."
-else
-    Warn "Autopath load balacing policy is not everywhere defined as SST:"
-    Comment "$(/sbin/autopath | grep "Load Balancing Policy" | uniq)"
-fi
+/sbin/autopath display | grep "Load Balancing Policy" | uniq | cut -d: -f2- | \
+    sed -e 's/^ //' > "$TMP_DIR/autopath.lbp"
+
+count=$(wc -l "$TMP_DIR/autopath.lbp" | awk '{ print $1 }')
+
+case $count in
+    1) grep -q "Shortest Service Time" "$TMP_DIR/autopath.lbp"
+       if (( $? = 0 )) ; then
+           Ok "Multipath LB Policy \"Shortest Service Time\" is in use."
+       else
+           Failed "Multipath LB Policy setting is not set to \"Shortest Service Time\"."
+	   Comment "Run: /sbin/autopath virtualdsf"
+	   Comment "     /sbin/autopath virtualdsf clean"
+	   Comment "     /sbin/autopath discover"
+	   Comment "     /sbin/autopath display | grep -e /dev/dsk | awk '{print $1}' | while read ; do"
+	   Comment "     /sbin/autopath set_lbpolicy SST $device"
+	   Comment "     done"
+       fi
+       ;;
+    *)  Warn "Multipath LB Policy settings should only have \"Shortest Service Time\"."
+	Comment "We found the following settings:"
+	Comment "$(cat $TMP_DIR/autopath.lbp)"
+	;;
+esac
+
---
+++ opt/criux/scripts/system/hp/42_check_scsimgr_lbp.sh
@@ -0,0 +1,25 @@
+# 42_check_scsimgr_lbp.sh
+# check the load balancing policy (on HP-UX 11.31)
+
+[[ ! -x /usr/sbin/scsimgr ]] && return   # if executable is not available we might be HP-UX 11.11 or 11.23
+
+/usr/sbin/scsimgr -p get_attr all_lun -a device_file -a load_bal_policy | grep disk |  cut -d: -f2 | \
+    uniq > "$TMP_DIR/multipath.lbp"
+
+count=$(wc -l "$TMP_DIR/multipath.lbp" | awk '{print $1}')
+
+case $count in
+    1) grep -q least_cmd_load "$TMP_DIR/multipath.lbp"
+       if (( $? = 0 )) ; then
+           Ok "Multipath LB Policy \"least_cmd_load\" is in use."
+       else
+           Failed "Multipath LB Policy setting is not set to \"least_cmd_load\"."
+	   Comment "Run: scsimgr save_attr -N \"/escsi/esdisk\" -a load_bal_policy=least_cmd_load;"
+       fi
+       ;;
+    *) Warn "Multipath LB Policy settings should only have \"least_cmd_load\"."
+       Comment "We found the following settings:"
+       Comment "$(cat $TMP_DIR/multipath.lbp)"
+       ;;
+esac
+
