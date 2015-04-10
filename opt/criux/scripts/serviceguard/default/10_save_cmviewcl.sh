1fa6b730ddb3f48eb1936306df3e890606509a16
Gratien D'haese <gratien.dhaese@hp.com>
2015-04-10 10:22:34 +0200

    adding $CMVIEWCL -f line -v output (default/10_save_cmviewcl.sh)

--- opt/criux/scripts/serviceguard/default/10_save_cmviewcl.sh
+++ opt/criux/scripts/serviceguard/default/10_save_cmviewcl.sh
@@ -1,3 +1,5 @@
 # 10_save_cmviewcl.sh
 $CMVIEWCL -v 2>/dev/null > "$TMP_DIR/cmviewcl.txt"
 LogPrintIfError "Could not create $TMP_DIR/cmviewcl.txt"
+$CMVIEWCL -f line -v 2>/dev/null > "$TMP_DIR/cmviewcl.line"
+LogPrintIfError "Could not create $TMP_DIR/cmviewcl.line"
