f5e1877e28d5cac4455456866583ef7f28017811
Gratien D'haese <gratien.dhaese@hp.com>
2015-04-10 10:54:11 +0200

    default/10_save_cmviewcl.sh: improved the error logging for cmviewcl -f line (older sg version cannot use this)

--- opt/criux/scripts/serviceguard/default/10_save_cmviewcl.sh
+++ opt/criux/scripts/serviceguard/default/10_save_cmviewcl.sh
@@ -2,4 +2,4 @@
 $CMVIEWCL -v 2>/dev/null > "$TMP_DIR/cmviewcl.txt"
 LogPrintIfError "Could not create $TMP_DIR/cmviewcl.txt"
 $CMVIEWCL -f line -v 2>/dev/null > "$TMP_DIR/cmviewcl.line"
-LogPrintIfError "Could not create $TMP_DIR/cmviewcl.line"
+LogPrintIfError "Could not create $TMP_DIR/cmviewcl.line due to -f line option not being supported."
