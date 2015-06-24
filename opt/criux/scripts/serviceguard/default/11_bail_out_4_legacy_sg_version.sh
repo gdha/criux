5f1f2493bef8b78131b944f24e57527ab13d25e5
Gratien D'haese <gratien.dhaese@hp.com>
2015-06-24 12:50:25 +0200

    - moved the 30_bail_out_4_legacy_sg_version.sh -> 11_bail_out_4_legacy_sg_version.sh - 20_grab_SG_patch.sh: using a method which works on Linux and HP-UX

--- opt/criux/scripts/serviceguard/default/11_bail_out_4_legacy_sg_version.sh
+++ opt/criux/scripts/serviceguard/default/11_bail_out_4_legacy_sg_version.sh
@@ -1,9 +1,10 @@
-# default/30_bail_out_4_legacy_sg_version.sh
+# default/11_bail_out_4_legacy_sg_version.sh
 # Currently we do not have the time to work only with cmgetconf -c cluster, and
 # cmgetconf -c cluster -p package (and that for each package).
-# SG 11.20 has the option to retrieve everything at once with cmviewcl -v -f line
+# SG 11.20 (and >) has the option to retrieve everything at once with cmviewcl -v -f line
 # As a consequence this means we cannot go deeper in SG_VERSION < A.11.20*
 case "$SG_VERSION" in
     A.11.20* ) : ;;  # we will continue
+    A.12.*   ) : ;;  # we will continue
     * ) Error "Serviceguard $SG_VERSION is legacy and we cannot dig deeper [sponsering needed]"
 esac
--- opt/criux/scripts/serviceguard/default/20_grab_SG_patch.sh
+++ opt/criux/scripts/serviceguard/default/20_grab_SG_patch.sh
@@ -1,5 +1,5 @@
 # 20_grab_SG_patch.sh
+# we will verify the SG patch level on this node; later on we will check if this comply with other nodes
+SG_PATCH=$( grep ^node $TMP_DIR/cmviewcl.line | grep $HOSTNAME | grep sg_patch | cut -d= -f2 )
 
-SG_PATCH=$( grep -i serviceguard $TMP_DIR/patches.output | grep PHSS | tail -1 | awk '{print $1}' )
 [[ -z "$SG_PATCH" ]] && SG_PATCH="none"
-
