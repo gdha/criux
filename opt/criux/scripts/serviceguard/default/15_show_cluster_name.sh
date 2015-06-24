95992210d83a705ce49b4b734981d27f311be21f
Gratien D'haese <gratien.dhaese@hp.com>
2015-06-24 10:26:22 +0200

    default/15_show_cluster_name.sh: simplified the grep

--- opt/criux/scripts/serviceguard/default/15_show_cluster_name.sh
+++ opt/criux/scripts/serviceguard/default/15_show_cluster_name.sh
@@ -1,5 +1,6 @@
 #  15_show_cluster_name.sh
-CL_NAME=$( head -3 $TMP_DIR/cmviewcl.txt | tail -1 | awk '{print $1}' )
+#CL_NAME=$( head -3 $TMP_DIR/cmviewcl.txt | tail -1 | awk '{print $1}' )
+CL_NAME=$( grep ^name $TMP_DIR/cmviewcl.line | cut -d= -f2 )
 
 if [[ -z "$CL_NAME" ]] ; then
     Error "Serviceguard cluster has no name!"
