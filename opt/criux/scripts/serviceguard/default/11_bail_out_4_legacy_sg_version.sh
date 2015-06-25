2d3a32204fc83c36ae78408cf5b8370741e98182
Gratien D'haese <gratien.dhaese@hp.com>
2015-06-25 10:35:30 +0200

    We will do our best to investigate A.11.18 and higher of SG

--- opt/criux/scripts/serviceguard/default/11_bail_out_4_legacy_sg_version.sh
+++ opt/criux/scripts/serviceguard/default/11_bail_out_4_legacy_sg_version.sh
@@ -4,7 +4,7 @@
 # SG 11.20 (and >) has the option to retrieve everything at once with cmviewcl -v -f line
 # As a consequence this means we cannot go deeper in SG_VERSION < A.11.20*
 case "$SG_VERSION" in
-    A.11.20* ) : ;;  # we will continue
+    A.11.18*|A.11.19*|A.11.20* ) : ;;  # we will continue
     A.12.*   ) : ;;  # we will continue
     * ) Error "Serviceguard $SG_VERSION is legacy and we cannot dig deeper [sponsering needed]"
 esac
